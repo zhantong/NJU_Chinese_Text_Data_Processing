2b4b0331d12e01cf9e1090d20fc10c7fd08bf276
zhantong1994 <zhantong1994@163.com>
2015-10-02 20:35:21 +0800

    删除无用代码，增加注释，整理格式

--- chinese_text_data_processing.py
+++ chinese_text_data_processing.py
@@ -24,9 +24,9 @@ class Process():
         判断用于保存结果的result是否存在，不存在则创建。
         """
         self.stop_words = set()  # 保存stop words
-        self.glo_count = {}  # 保存文件中每个词语出现的行数（区别于次数）
+        self.glo_count = {}  # 保存所有文件中每个词语出现的行数（区别于次数）
         self.result = []  # 保存最终结果
-        self.line_all=0
+        self.line_all = 0  # 保存所有文件总行数
         self.get_stop_words()  # 初始化时即读取stop words
         if not os.path.exists('result'):  # 保存结果文件的目录不存在则创建
             os.mkdir('result')
@@ -43,13 +43,13 @@ class Process():
         """分词并整理，消除stop words，计算词语出现次数
         将jieba分词得到每个单词加入到dict，以单词为key，value为dict
         self.result如[{{'a':{'count':1},...},'count_all':50},...]
-        此外还会记录每行text的词语总数，临时变量count
-        以及每个词语共出现在多少行text中，保存到glo_count
+        此外还会记录每行text的消除stop words后的词语总数，临时变量count
+        如果某行出现某词，则在glo_count中对应加1，以计算词语出现总行数
         """
-        res=[]
+        res = []  # 每个文件临时结果
         with open('lily/' + file_name, 'r', encoding='utf-8') as f:
             for line in f:
-                self.line_all+=1
+                self.line_all += 1  # 总行数
                 dic = {}  # 记录每一行的词语
                 count = 0  # 每行中词语总数
                 s = jieba.cut(line.strip())
@@ -64,7 +64,7 @@ class Process():
                             }
                             if not item in self.glo_count:  # 不在glo_count中则首先创建
                                 self.glo_count[item] = 0
-                            self.glo_count[item] += 1  # 更新词语出现行数
+                            self.glo_count[item] += 1  # 更新词语出现行数，只在此行中第一次出现时增加
                         dic[item]['count'] += 1  # 更新词语出现次数
                         count += 1  # 更新每行词语总数
                 res.append({
@@ -72,56 +72,41 @@ class Process():
                     'count_all': count
                 })
         self.result.append({
-            'content':res,
-            'file_name':file_name
-            })
+            'content': res,
+            'file_name': file_name
+        })
 
     def cal(self):
         """计算tf-idf
-        通过分词得到的每行text中各词语出现次数，
-        每行text词语总数，
-        每个词语出现text行数，
+        通过分词得到的每行中各词语出现次数，
+        每行词语总数，
+        每个词语出现行数，
         总行数，
         可以计算得到tf-idf。
         """
         for f in self.result:
-
-            #lines = len(f['content'])  # 总行数
             for line in f['content']:
                 tokens = line['result']
                 count_all = line['count_all']  # 每行词语总数
                 for token, values in tokens.items():
                     values['tf'] = values['count'] / count_all  # 计算tf
-                    values['idf'] = math.log(
-                        self.line_all / self.glo_count[token])  # 计算idf
+                    values['idf'] = 1 + math.log(
+                        self.line_all / self.glo_count[token])  # 计算idf，加1避免idf为0
                     values['tf-idf'] = values['tf'] * values['idf']  # 计算tf-idf
-
 
     def write(self):
         """形成td-idf值矩阵，将结果写入文件
         """
         all_tokens = sorted(self.glo_count)  # 避免每次遍历时dict随机排序
         for doc in self.result:
-            with open('result/'+doc['file_name'],'w',encoding='utf-8') as f:
+            with open('result/' + doc['file_name'], 'w', encoding='utf-8') as f:
                 for line in doc['content']:
-                    for token in all_tokens:
+                    for token in all_tokens:  # 以全部分词序列
                         if token in line['result']:
-                            f.write('%.4f\t'%line['result'][token]['tf-idf'])
+                            f.write('%.4f\t' % line['result'][token]['tf-idf'])
                         else:
                             f.write('0\t')
                     f.write('\n')
-        print(len(self.glo_count))
-
-    def start_once(self, file_name):
-        """单次完整运行
-        只处理一个文件，
-        每次开始处理前初始化变量。
-        """
-        #self.glo_count = {}  # 重新初始化变量
-        #self.result = []
-        self.tokenization(file_name)
-        self.cal()
-        self.sort_and_write(file_name)
 
     def start(self):
         """多文件完整运行
